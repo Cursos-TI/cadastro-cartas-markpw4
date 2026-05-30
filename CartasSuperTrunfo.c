8205114ec2eaf3a297c563bfab3675a97a4fa708
markpw4 <marcossouzaport@gmail.com>
2026-05-30 20:20:23 +0000

    adicionei printf no codigo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,10 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+printf ("Olá Mundo no codespace\n");
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
 
 return 0;
-} 
+}  
